f299e5893bdec358add01907852616d73e9d1990
Jeff <admin@braceyourselv.es>
2020-08-22 11:05:34 +0200

    changing backend framework to express

--- backend/app/asgi.py
+++ backend/app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for backend project.
+ASGI config for app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- backend/app/wsgi.py
+++ backend/app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for backend project.
+WSGI config for app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
