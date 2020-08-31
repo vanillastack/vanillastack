063f37d191a495099ff9d12a1ba3b002138b2fb2
Karsten Samaschke <karsten.samaschke@cloudical.io>
2020-08-31 07:17:16 +0200

    Changed screen layout, added logo

--- backend/src/frontend/Start.vue
+++ backend/src/frontend/Start.vue
@@ -2,12 +2,13 @@
   <span>
     <!-- Header area -->
     <header class="header">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <router-link to="/" class="navbar-brand"><h1>VanillaStack</h1></router-link>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <router-link to="/" class="navbar-brand"><img src="/images/vanilla-logo.png" class="logo" /></router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
+        <!--
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
@@ -18,6 +19,7 @@
             </li>
           </ul>
         </div>
+        -->
       </nav>
     </header>
 
@@ -25,7 +27,17 @@
     <main class="main h-100">
       <div class="container-fluid h-100">
         <div class="row h-100">
-          <div class="col navigation h-100"><h3>Navigation</h3></div>
+          <div class="col navigation h-100">
+            <!-- Local navigation for the installer -->
+            <ul class="secondary-nav">
+              <li><router-link to="/start">Start</router-link></li>
+              <li><router-link to="/terms">Terms</router-link></li>
+              <li><router-link to="/kind">Installation Kind</router-link></li>
+              <li><router-link to="/key">Public Key</router-link></li>
+              <li><router-link to="/nodes">Nodes</router-link></li>
+              <li><router-link to="/summary">Summary</router-link></li>
+            </ul>
+          </div>
           <div class="col-11"><router-view></router-view></div>
         </div>
       </div>
