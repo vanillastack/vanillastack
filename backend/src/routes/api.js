25dfcf2c2ffe54e1482ff74105a9e5b1b8e12cfa
JazzyJeff <jeff.chousterman@cloudical.io>
2020-09-08 23:21:20 +0200

    rebuilding docker container; added nginx proxy; added frontend build

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const connectionRouter = require('./api/connection');
 const setupRouter = require('./api/setup')
 const infoRouter = require('./api/info');
+const docuRouter = require('./openapi');
 
 router.use('/connection', connectionRouter);
 router.use('/setup', setupRouter);
 router.use('/info', infoRouter);
+router.use('/api-docs', docuRouter);
 
 router.get('/', function (req, res) {
     res.send('Hello World');
