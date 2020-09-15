db55eec485241840c1873e5a890bafee8db52993
Jan Walzer <github_jan.w@lzer.net>
2020-09-15 18:56:32 +0200

    FIX: select the correct container image to start from

--- live_image/rootfs/live-build/config/includes.chroot/usr/local/bin/run_containerimage.bash
+++ live_image/rootfs/live-build/config/includes.chroot/usr/local/bin/run_containerimage.bash
@@ -3,4 +3,4 @@
 echo "decompressing container image"
 xz -c -v -d < "$1" | docker load
 echo "starting container"
-/usr/bin/docker run --rm --name vanilla-installa -p 8080:8080 harbor.cloudical.net/vanillastack/vsinstaller
+/usr/bin/docker run --rm --name vanilla-installa -p 8080:8080 harbor.cloudical.net/vanillastack/installer
