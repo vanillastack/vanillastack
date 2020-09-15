0d23791e5ba3d354787b10fca40feaf5218d6e11
Jan Walzer <github_jan.w@lzer.net>
2020-09-15 09:50:14 +0200

    CHG: push to s3 optimized

--- live_image/rootfs/build-installer.bash
+++ live_image/rootfs/build-installer.bash
@@ -72,13 +72,20 @@ lb build 2>&1 | tee -a $OUTPUT/build.log
 
 cp vanillastack-installer* $OUTPUT
 
-mcli config host add vanilla https://s3.cloudical.net \
-  $(echo $AWS_ACCESS_KEY_ID | base64 -d) \
-  $(echo $AWS_SECRET_ACCESS_KEY | base64 -d) \
-  && for I in $OUTPUT/vanillastack-installer*;
-      do
-        mcli cp $I vanilla/vanillastack-downloads-bkt-3d791dcf-fb62-49c7-a4cf-b153203e3ff2/iso/ || :
-      done
+if [[ -n "$AWS_SECRET_ACCESS_KEY" ]]
+  then
+    branch="pure_testing/"
+    [[ -n "$CI_COMMIT_REF_NAME" ]] && branch="$CI_COMMIT_REF_NAME/"
+    [[ "$branch" == "master/" ]] && branch=""
+
+    mcli config host add vanilla https://s3.cloudical.net \
+      $(echo $AWS_ACCESS_KEY_ID | base64 -d) \
+      $(echo $AWS_SECRET_ACCESS_KEY | base64 -d) \
+      && for I in $OUTPUT/vanillastack-installer*;
+          do
+            mcli cp $I vanilla/vanillastack-downloads-bkt-3d791dcf-fb62-49c7-a4cf-b153203e3ff2/iso/$branch || :
+          done
+  fi
 #
 # Content will be available via https://s3.cloudical.net/vanillastack-downloads-bkt-3d791dcf-fb62-49c7-a4cf-b153203e3ff2
 
