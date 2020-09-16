89448253b55f65fb95943e75b063a8f4a1e7af62
JazzyJeff <jeff.chousterman@cloudical.io>
2020-09-16 10:40:05 +0200

    fixing bug on setup

--- backend/src/routes/api/setup.js
+++ backend/src/routes/api/setup.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const {getClient, setup, sleep, genTransactionId, randPassword} = require('../../websocket');
 
 /**
@@ -74,7 +75,7 @@ router.post('/', function (req, res) {
     }
 
 
-    const basePath = `/usr/workdir/ansible/`;
+    const basePath = path.join(__dirname, '../../../ansible');
     // Building Inventory
     const hostsYaml = {
         all: {
