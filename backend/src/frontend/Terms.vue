296314f0e64969752b95c53876802e06653563b8
Karsten Samaschke <karsten.samaschke@cloudical.io>
2020-10-02 18:36:52 +0200

    CHG Correct a small typo

--- backend/src/frontend/Terms.vue
+++ backend/src/frontend/Terms.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row margin-2em">
             <div class="col">
-                <h3>Terms and Conditions</h3>
+                <h3>VanillaStack 20.09.01: Terms and Conditions</h3>
             </div>
         </div>
         <div class="row margin-2em">
@@ -12,17 +12,15 @@
         </div>
         <div class="row margin-2em">
             <div class="col cite">
-                <p>
-                    <h5>Copyright 2020 Cloudical Deutschland GmbH</h5>
-                    <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this product except in compliance with the License.</p>
-                    <p>You may obtain a copy of the License at</p>
-                    <p><a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">http://www.apache.org/licenses/LICENSE-2.0</a></p>
-                    <p>Unless required by applicable law or agreed to in writing, software
-                    distributed under the License is distributed on an "AS IS" BASIS,
-                    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-                    See the License for the specific language governing permissions and
-                    limitations under the License.</p>
-                </p>
+                <h5>Copyright 2020 Cloudical Deutschland GmbH</h5>
+                <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this product except in compliance with the License.</p>
+                <p>You may obtain a copy of the License at</p>
+                <p><a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">http://www.apache.org/licenses/LICENSE-2.0</a></p>
+                <p>Unless required by applicable law or agreed to in writing, software
+                distributed under the License is distributed on an "AS IS" BASIS,
+                WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                See the License for the specific language governing permissions and
+                limitations under the License.</p>
             </div>
         </div>
         <form>
