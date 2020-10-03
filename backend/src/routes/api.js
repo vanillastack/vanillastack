a1340e1f278cac461335480ace42a1a44f6d6b8f
Jeff <jeff.chousterman@cloudical.io>
2020-10-03 09:22:54 +0000

    Revert "Merge branch 'ansible_config_export' into 'master'"
    This reverts merge request !86

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -4,14 +4,12 @@ const router = express.Router();
 const connectionRouter = require('./api/connection');
 const setupRouter = require('./api/setup');
 const kubeConfigRouter = require('./api/kubeconfig');
-const inventoryRouter = require('./api/inventory');
 const infoRouter = require('./api/info');
 const docuRouter = require('./openapi');
 
 router.use('/connection', connectionRouter);
 router.use('/setup', setupRouter);
 router.use('/config', kubeConfigRouter);
-router.use('/inventory', inventoryRouter);
 router.use('/info', infoRouter);
 router.use('/api-docs', docuRouter);
 
