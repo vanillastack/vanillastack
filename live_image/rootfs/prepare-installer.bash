d8cee6bd9c4dccf925102475234dddc76c04f061
Jan Walzer <github_jan.w@lzer.net>
2020-10-28 13:17:14 +0100

    WIP on live-image: c076475 Merge branch 'master' into live-image

--- live_image/rootfs/prepare-installer.bash
+++ live_image/rootfs/prepare-installer.bash
@@ -40,18 +40,18 @@ sed -i '1161s%umount%#umount%' /usr/share/debootstrap/functions
 mkdir -p ${WORKDIR}
 mkdir -p ${OUTPUT}
 
-echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" > /etc/apt/sources.list.d/docker.list
-
 # Docker Repo
+#echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" > /etc/apt/sources.list.d/docker.list
 curl -fsSL https://download.docker.com/linux/debian/gpg > $WORKDIR/live-build/config/archives/docker.key.chroot
-cat $WORKDIR/live-build/config/archives/docker.key.chroot | apt-key add -
+#cat $WORKDIR/live-build/config/archives/docker.key.chroot | apt-key add -
 
 # Skopeo Repo
 echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/Debian_10/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 curl -L 'https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/Debian_10/Release.key' | apt-key add -
 
 
-apt-get update && apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io skopeo
+#apt-get update && apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io skopeo
+apt-get update && apt-get install -y --no-install-recommends skopeo
 
 wget https://dl.min.io/client/mc/release/linux-amd64/mc -O /usr/local/bin/mcli
 chmod +x /usr/local/bin/mcli
